c80bb23aed501d06384528e26bd3fd1909f9029b
moein najafi <test@gmail.com>
2023-08-05 08:39:23 +0330

    add Clinic Model

---
+++ clinic/managment/models/Clinic.py
@@ -0,0 +1,12 @@
+from django.db import models
+from User import User
+
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=30,blank=False,unique=True,null=False)
+    address = models.CharField(max_length=100,blank=True,null=True)
+    manager = models.ForeignKey(User,to_field='username',on_delete=models.DO_NOTHING,blank=True,null=True)
+
+
+    def __str__(self):
+        return self.name
