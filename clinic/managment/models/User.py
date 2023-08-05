fd3189fbdc198c5dadbff169e2a3d4ed58bfedac
moein najafi <test@gmail.com>
2023-08-05 08:25:46 +0330

    User Created

---
+++ clinic/managment/models/User.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    TYPE_CHOICES = (
+        ('Clinic_Admin', 'CA'),
+        ('Typical_User', 'TU'),
+        ('Nurse', 'N'),
+        ('Sick', 'S'),
+    )
+
+    user_type = models.CharField(
+        max_length=12,
+        choices=TYPE_CHOICES,
+        blank=True,
+        null=True,
+    )
+
+
+    # REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.username
