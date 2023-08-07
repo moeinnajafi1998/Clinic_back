e54b33a458ea4e0cebd6968dd724e1fc982eecfc
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-07 17:08:16 +0330

    improvements

--- clinic/managment/models/Category.py
+++ clinic/managment/models/Category.py
@@ -2,6 +2,17 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=30,blank=False,unique=True,null=False)
+    TYPE_CHOICES = (
+        ('Professional', 'P'),
+        ('General', 'G'),
+    )
+
+    type = models.CharField(
+        max_length=12,
+        choices=TYPE_CHOICES,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.name
