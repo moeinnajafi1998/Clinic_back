b6cbdb6e16964493037bcbc9b97d6ac221a53966
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-14 17:54:52 +0330

    Edit Item Model

--- clinic/managment/models/Clinic.py
+++ clinic/managment/models/Clinic.py
@@ -6,7 +6,5 @@ class Clinic(models.Model):
     name = models.CharField(max_length=30,blank=False,unique=True,null=False)
     address = models.CharField(max_length=100,blank=True,null=True)
     manager = models.ForeignKey(User,to_field='username',on_delete=models.SET_NULL,blank=True,null=True)
-
-
     def __str__(self):
         return self.name
--- clinic/managment/models/Item.py
+++ clinic/managment/models/Item.py
@@ -7,3 +7,8 @@ class Item(models.Model):
     name = models.CharField(max_length=30,blank=False,unique=True,null=False)
     category = models.ForeignKey(Category,to_field='name',blank=True,null=True,on_delete=models.SET_NULL)
     wharehouse =  models.ForeignKey(WhareHouse,to_field='name',blank=True,null=True,on_delete=models.SET_NULL)
+    number = models.IntegerField(blank=True,null=True,default=0)
+    image = models.ImageField(upload_to='items/',null=True,blank=True)
+
+    def __str__(self):
+        return self.name
