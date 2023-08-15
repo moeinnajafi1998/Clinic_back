77436a18b5fa3b58321b07a9b6aa80be696ad187
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-15 10:18:12 +0330

    bug fix

--- clinic/managment/models/Item.py
+++ clinic/managment/models/Item.py
@@ -4,7 +4,7 @@ from .WhareHouse import WhareHouse
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=30,blank=False,unique=True,null=False)
+    name = models.CharField(max_length=30,blank=False,unique=False,null=False)
     category = models.ForeignKey(Category,to_field='name',blank=True,null=True,on_delete=models.SET_NULL)
     wharehouse =  models.ForeignKey(WhareHouse,to_field='name',blank=True,null=True,on_delete=models.SET_NULL)
     number = models.IntegerField(blank=True,null=True,default=0)
