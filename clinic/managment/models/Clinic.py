53de1591d5f0b5756f6a8a9fe171d534c63f6910
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-15 14:52:14 +0330

    Edit Clinic Model

--- clinic/managment/models/Clinic.py
+++ clinic/managment/models/Clinic.py
@@ -5,6 +5,8 @@ from .User import User
 class Clinic(models.Model):
     name = models.CharField(max_length=30,blank=False,unique=True,null=False)
     address = models.CharField(max_length=100,blank=True,null=True)
-    manager = models.ForeignKey(User,to_field='username',on_delete=models.SET_NULL,blank=True,null=True)
+    manager = models.ForeignKey(User,to_field='username',related_name="manager",on_delete=models.SET_NULL,blank=True,null=True)
+    typical_user = models.ForeignKey(User,to_field='username',related_name="secratary",on_delete=models.SET_NULL,blank=True,null=True)
+
     def __str__(self):
         return self.name
