10533a07f523879d3319609c6d7fef8da4355831
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-16 16:58:05 +0330

    improving request session model

--- clinic/registeration/models/request_session.py
+++ clinic/registeration/models/request_session.py
@@ -4,7 +4,7 @@ from django.db import models
 class RequestSession(models.Model):
     sick = models.CharField(max_length=50,blank=False,unique=False,null=False)
     clinic = models.CharField(max_length=50,blank=False,unique=False,null=False)
-    description = models.TextField()
+    description = models.TextField(blank=True,null=True)
     is_checked = models.BooleanField(default=False)
 
     def __str__(self):
