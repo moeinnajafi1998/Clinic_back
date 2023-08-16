255336ad712a425c0f69f8bc168e438dceff12c0
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-16 08:39:28 +0330

    admin.py for registeration app

--- clinic/registeration/admin.py
+++ clinic/registeration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class RequestSessionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(RequestSession,RequestSessionAdmin)
